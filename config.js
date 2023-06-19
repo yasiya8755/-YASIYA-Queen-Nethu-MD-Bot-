38904612f7a496ec57a3100f6516291bc2df524a
yasiya8755 <133846874+yasiya8755@users.noreply.github.com>
2023-06-19 12:49:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,28 +4,28 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.mongodb = process.env.MONGODB_URI || "mongodb://ubiqwxgyexyet56klalu:HcM170fy413dYc1HyrL@bnnnrpamriobo4hkg9cs-mongodb.services.clever-cloud.com:2396/bnnnrpamriobo4hkg9cs"
 global.port= process.env.PORT || 5000
 global.email = 'shashikachirath1@gmail.com'
 global.github = 'https://github.com/mrhansamala/-Queen-Nethu-MD-Bot-'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94781708673'
-global.devs = '94781708673';
+global.sudo = process.env.SUDO || '94743548986'
+global.devs = '94743548986';
 global.website = 'https://github.com/mrhansamala/-Queen-Nethu-MD-Bot-' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/d229929fd0e975d64010c.jpg'
 module.exports = {
   botname: process.env.BOT_NAME || 'Queen Nethu',
-  ownername:process.env.OWNER_NAME || 'MR.Hansamala',
+  ownername:process.env.OWNER_NAME || 'MR.Yasiya',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
   packname: process.env.PACK_INFO.split(";")[1] || 'Name',
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
-  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'true',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
-  readmessage: process.env.READ_MESSAGE || false,
+  readmessage: process.env.READ_MESSAGE || true,
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
@@ -33,7 +33,7 @@ module.exports = {
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
   BRANCH: 'main',
-  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || 'I am Onlin Now',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'OPENAI_KEY',
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR',
